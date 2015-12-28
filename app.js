c39e4d2e83abe31eeb3f14fe3c91cdc2c7e46bfa
kerozene <keroschene@gmail.com>
2015-12-28 19:37:20 +1100

    Start repl when in dev mode

--- app.js
+++ app.js
@@ -4,12 +4,21 @@
  * @author Teemu Lahti <teemu.lahti@gmail.com>
  * @version 0.6.0
  */
-console.log('Cards Against Humanity IRC bot');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+
+var repl = (process.env.NODE_ENV === 'development') ? require('repl').start('> ') : { context: {} };
+
+console.log('Cards Against Humanity IRC bot');
+
 
 var Bot = require('./app/bot'),
     bot = new Bot();
 
+repl.context.repl = repl;
+repl.context.bot  = bot;
+repl.context.config = bot.config;
+repl.context.client = bot.client;
+repl.context.cah    = bot.cah;
 
 bot.connect();
